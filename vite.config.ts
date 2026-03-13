b7baa97130fd968026fe414cc0848bffb37b1187
Illia Yershov <illia.yershov@gmail.com>
2026-03-13 15:46:52 +0100

    Remove unsafe manual vendor chunking

--- vite.config.ts
+++ vite.config.ts
@@ -4,40 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (!id.includes('node_modules')) return undefined
-
-          if (
-            id.includes('/react/')
-          ) {
-            return 'react-vendor'
-          }
-
-          if (id.includes('/react-dom/')) {
-            return 'react-dom-vendor'
-          }
-
-          if (id.includes('/react-router-dom/')) {
-            return 'router-vendor'
-          }
-
-          if (
-            id.includes('/@chakra-ui/') ||
-            id.includes('/@emotion/')
-          ) {
-            return 'chakra-vendor'
-          }
-
-          if (id.includes('/react-icons/')) {
-            return 'icons-vendor'
-          }
-
-          return undefined
-        },
-      },
-    },
-  },
 })
